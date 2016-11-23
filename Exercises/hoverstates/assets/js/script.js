f2ec4b6e2a0c64d765c8bb9626af733da9aa0969
onissachar <asachar@cca.edu>
2016-11-22 17:46:13 -0800

    hoooover

---
+++ Exercises/hoverstates/assets/js/script.js
@@ -0,0 +1,40 @@
+$('document').ready(function(){
+
+
+
+
+
+	$('.upper').mouseenter(function() {
+		$('.lower').fadeOut(1000);
+	});
+
+	$('.upper').mouseenter(function() {
+		$('.upper').fadeOut(1000);
+	});
+
+	$('.lower').mouseenter(function() {
+	$('.lower').fadeIn(10);
+	});
+
+	$('.lower').mouseenter(function() {
+	$('.upper').fadeIn(10);
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
