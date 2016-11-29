68729800ef6bc41bee773ec4a5ec47fabefd9da0
onissachar <asachar@cca.edu>
2016-11-28 21:25:52 -0800

    mm

---
+++ Projects/Microsite/assets/js/script.js
@@ -0,0 +1,229 @@
+$('document').ready(function(){
+
+
+
+
+$('.s1crop').hover(function() {
+$('.s1full').show();
+$('.s1full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.s1full').hide();
+$('.container').css("opacity", "1")
+});
+
+
+$('.i1crop').hover(function() {
+$('.i1full').show();
+$('.i1full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.i1full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.s2crop').hover(function() {
+$('.s2full').show();
+$('.s2full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.s2full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.tercrop').hover(function() {
+$('.terfull').show();
+$('.terfull').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.terfull').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.c1crop').hover(function() {
+$('.c1full').show();
+$('.c1full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.c1full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.orcrop').hover(function() {
+$('.orfull').show();
+$('.orfull').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.orfull').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.i2crop').hover(function() {
+$('.i2full').show();
+$('.i2full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.i2full').hide();
+$('.container').css("opacity", "1")
+});
+
+
+$('.t1crop').hover(function() {
+$('.t1full').show();
+$('.t1full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.t1full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.a1crop').hover(function() {
+$('.a1full').show();
+$('.a1full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.a1full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.k1crop').hover(function() {
+$('.k1full').show();
+$('.k1full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.k1full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.encrop').hover(function() {
+$('.enfull').show();
+$('.enfull').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.enfull').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.t2crop').hover(function() {
+$('.t2full').show();
+$('.t2full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.t2full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.a2crop').hover(function() {
+$('.a2full').show();
+$('.a2full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.a2full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.c2crop').hover(function() {
+$('.c2full').show();
+$('.c2full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.c2full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.t3crop').hover(function() {
+$('.t3full').show();
+$('.t3full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.t3full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.ivcrop').hover(function() {
+$('.ivfull').show();
+$('.ivfull').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.ivfull').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.iscrop').hover(function() {
+$('.isfull').show();
+$('.isfull').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.isfull').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.m1crop').hover(function() {
+$('.m1full').show();
+$('.m1full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.m1full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.a3crop').hover(function() {
+$('.a3full').show();
+$('.a3full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.a3full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.ndcrop').hover(function() {
+$('.ndfull').show();
+$('.ndfull').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.ndfull').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.p1crop').hover(function() {
+$('.p1full').show();
+$('.p1full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.p1full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.l1crop').hover(function() {
+$('.l1full').show();
+$('.l1full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.l1full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.aycrop').hover(function() {
+$('.ayfull').show();
+$('.ayfull').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.ayfull').hide();
+$('.container').css("opacity", "1")
+});
+
+$('.f1crop').hover(function() {
+$('.f1full').show();
+$('.f1full').css("display", "inline");
+$('.container').css("opacity", "0")
+}, function() {
+$('.f1full').hide();
+$('.container').css("opacity", "1")
+});
+
+$('button').on('click', function(){
+$('#slider').toggleClass('open');
+
+})
+
+});
